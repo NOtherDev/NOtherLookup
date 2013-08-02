f98c258caf6fb8bf5cb176a029c2b4f02159abb0
Adam Bar <adamrbar@gmail.com>
2013-08-02 22:09:50 +0200

    Where implemented

--- NOtherLookup.Tests/Lookup_Where.cs
+++ NOtherLookup.Tests/Lookup_Where.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Machine.Specifications;
 using NOtherLookup.Tests.Utils;
 
@@ -15,16 +14,46 @@ namespace NOtherLookup.Tests
                 .WithKey("b", new[] { 2, 4 }).Build();
 
         Because of = () => 
-            filtered = lookup.Where(x => x <= 2);
+            filtered = lookup.Where(x => x > 3);
+
+        It should_filter_out_values_according_to_predicate = () =>
+        {
+            filtered.Count.ShouldEqual(1);
+            filtered["b"].ShouldContainExactly(4);
+        };
 
         private static ILookup<string, int> lookup, filtered;
     }
 
-    public static class X
+    public class When_filtering_null_lookup_by_values
     {
-        public static ILookup<K,V> Where<K,V>(this ILookup<K,V> t, Func<V, bool> p)
-        {
-            return t;
-        } 
+        Establish context = () =>
+            lookup = null;
+
+        Because of = () => 
+            exception = Catch.Exception(() => lookup.Where(x => x > 3));
+
+        It should_throw_ArgumentNullException = () =>
+            exception.ShouldBeOfType<ArgumentNullException>();
+
+        private static ILookup<string, int> lookup;
+        private static Exception exception;
+    }
+
+    public class When_filtering_lookup_by_null_predicate
+    {
+        Establish context = () =>
+            lookup = LookupBuilder
+                .WithKey("a", new[] { 1, 3 })
+                .WithKey("b", new[] { 2, 4 }).Build();
+
+        Because of = () => 
+            exception = Catch.Exception(() => lookup.Where(null));
+
+        It should_throw_ArgumentNullException = () =>
+            exception.ShouldBeOfType<ArgumentNullException>();
+
+        private static ILookup<string, int> lookup;
+        private static Exception exception;
     }
 }
---
+++ NOtherLookup/Lookup_Where.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NOtherLookup
+{
+    public static partial class LookupExtensions
+    {
+        public static ILookup<TKey, TValue> Where<TKey, TValue>(this ILookup<TKey, TValue> lookup, Func<TValue, bool> valuePredicate)
+        {
+            if (lookup == null)
+                throw new ArgumentNullException("lookup");
+
+            return lookup
+                .Select(x => new KeyValuePair<TKey, IEnumerable<TValue>>(x.Key, x.Where(valuePredicate)))
+                .ToLookup();
+        }
+    }
+}
