fcd34c78193fd2c25589df46c3124b621c374fdd
Adam Bar <adamrbar@gmail.com>
2014-01-05 15:03:16 +0100

    Zip

---
+++ NOtherLookup.Tests/Lookup_Zip.cs
@@ -0,0 +1,83 @@
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using Machine.Specifications;
+using NOtherLookup.Tests.Utils;
+
+namespace NOtherLookup.Tests
+{
+    public class When_zipping_lookups
+    {
+        Establish context = () =>
+            lookup = LookupBuilder
+                .WithKey(1, new[] { 1, 2 })
+                .WithKey(2, new[] { 3, 4, 4 }).Build();
+
+        Because of = () =>
+            zipped = lookup.Zip(LookupBuilder
+                .WithKey(2, new[] { "e", "d" })
+                .WithKey(3, new[] { "f", "g" }).Build(), (x, y) => x + y);
+
+        It should_create_lookup_with_keys_from_first_lookup_that_has_matching_keys_in_second_lookup = () =>
+            zipped.Single().Key.ShouldEqual(2);
+
+        It should_have_elements_zipped_according_to_selector_up_to_shorters_collection_length = () => 
+            zipped[2].ShouldContainExactly("3e", "4d");
+        
+        private static ILookup<int, int> lookup;
+        private static ILookup<int, string> zipped;
+    }
+    
+    public class When_zipping_null_with_lookup
+    {
+        Establish context = () =>
+            lookup = null;
+
+        Because of = () =>
+            exception = Catch.Exception(() => lookup.Zip(LookupBuilder
+                .WithKey(2, new[] { "e", "d" })
+                .WithKey(3, new[] { "f", "g" }).Build(), (x, y) => x + y));
+
+        It should_throw_ArgumentNullException = () =>
+            exception.ShouldBeOfType<ArgumentNullException>();
+
+        private static ILookup<int, string> lookup;
+        private static Exception exception;
+    }
+    
+    public class When_zipping_lookup_with_null
+    {
+        Establish context = () =>
+            lookup = LookupBuilder
+                .WithKey(1, new[] { "a", "b" })
+                .WithKey(2, new[] { "c", "d" }).Build();
+
+        Because of = () =>
+            exception = Catch.Exception(() => lookup.Zip<int, string, int, string>(null, (x, y) => x + y));
+
+        It should_throw_ArgumentNullException = () =>
+            exception.ShouldBeOfType<ArgumentNullException>();
+
+        private static ILookup<int, string> lookup;
+        private static Exception exception;
+    }
+
+    public class When_zipping_lookups_with_null_result_selector
+    {
+        Establish context = () =>
+            lookup = LookupBuilder
+                .WithKey(1, new[] { 1, 2 })
+                .WithKey(2, new[] { 3, 4, 4 }).Build();
+
+        Because of = () =>
+            exception = Catch.Exception(() => lookup.Zip(LookupBuilder
+                .WithKey(2, new[] { "e", "d" })
+                .WithKey(3, new[] { "f", "g" }).Build(), (Func<int, string, string>) null));
+
+        It should_throw_ArgumentNullException = () =>
+            exception.ShouldBeOfType<ArgumentNullException>();
+
+        private static ILookup<int, int> lookup;
+        private static Exception exception;
+    }
+}
---
+++ NOtherLookup/Lookup_Zip.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace NOtherLookup
+{
+    public static partial class LookupExtensions
+    {
+        public static ILookup<TKey, TResult> Zip<TKey, TFirst, TSecond, TResult>(this ILookup<TKey, TFirst> first,
+            ILookup<TKey, TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            if (first == null)
+                throw new ArgumentNullException("first");
+            if (second == null)
+                throw new ArgumentNullException("second");
+            if (resultSelector == null)
+                throw new ArgumentNullException("resultSelector");
+
+            return first.ZipImpl(second, resultSelector).ToLookup();
+        }
+
+        private static IEnumerable<KeyValuePair<TKey, IEnumerable<TResult>>> ZipImpl<TKey, TFirst, TSecond, TResult>(
+            this ILookup<TKey, TFirst> first,
+            ILookup<TKey, TSecond> second, 
+            Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            foreach (var grouping in first)
+            {
+                using (var iterator1 = grouping.GetEnumerator())
+                {
+                    var key = grouping.Key;
+                    var values = new List<TResult>();
+
+                    using (var iterator2 = second[key].GetEnumerator())
+                    {
+                        while (iterator1.MoveNext() && iterator2.MoveNext())
+                        {
+                            values.Add(resultSelector(iterator1.Current, iterator2.Current));
+                        }
+                    }
+
+                    yield return new KeyValuePair<TKey, IEnumerable<TResult>>(key, values);
+                }
+            }
+        }
+    }
+}
